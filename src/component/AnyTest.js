22f66629a384a4dcd3e591a4fe22e262757dd3eb
v_wangxiaolong <v_wangxiaolong01@baidu.com>
2018-09-13 15:51:31 +0800

    方法里面可以直接return jsx 语法

--- src/component/AnyTest.js
+++ src/component/AnyTest.js
@@ -11,16 +11,22 @@ class GetParams extends Component {
     constructor(props) {
         super(props);
         this.myRef = React.createRef();
-        this.refsClick = this.refsClick.bind(this)
+        this.refsClick = this.refsClick.bind(this);
+        // this.getP = this.getP.bind(this);
     }
 
     refsClick() {
-        console.log(this.myRef.current, 'myref')
+        console.log(this.myRef.current, 'myref');
+    }
+
+    getP() {
+        return <p>这是一个段落</p>;
     }
 
     render() {
         return (
             <div className="App">
+                { this.getP() }
                 <button ref={this.myRef} onClick={this.refsClick}>点击</button>
             </div>
         );
